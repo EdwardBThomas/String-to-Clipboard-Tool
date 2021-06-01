c956dfd9c9fb2be3a167b44ac110fd98c35e3776
EdwardBThomas <81997990+EdwardBThomas@users.noreply.github.com>
2021-05-31 22:55:43 -0700

    Update String-to-Clipboard_Tool_MR.ps1

--- String-to-Clipboard_Tool_MR.ps1
+++ String-to-Clipboard_Tool_MR.ps1
@@ -9,7 +9,7 @@ Figure out how to:
 function SCT 
 {
 {
-$scttable = (Get-Content C:\Users\ebtho\OneDrive\Documents\PowerShell\SCTTableTXT-JSON.txt | ConvertFrom-JSON -AsHashtable)
+$scttable = (Get-Content .\SCTTableTXT-JSON.txt | ConvertFrom-JSON -AsHashtable)
 }
 
 [string]$sctinputStr = Read-Host "Type shorthand, press enter." 
